d3a50981d08dd171342786049bf574de88ad8e37
tais defante <taisadefante@hotmail.com>
2024-01-31 19:07:12 -0300

    style

--- src/components/Lista/index.js
+++ src/components/Lista/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.scss";
 
 function Lista() {
   const tarefas = [
@@ -12,11 +13,11 @@ function Lista() {
     },
   ];
   return (
-    <aside>
+    <aside className="listaTarefas">
       <h2>Estudo do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index}>
+          <li key={index} className="item">
             <h3> {item.tarefa}</h3>
             <spam>{item.tempo}</spam>
           </li>
